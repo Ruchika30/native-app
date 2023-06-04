3990aa2b5262a97470cf45b6c81da9317e48d871
Ruchika30 <sood.ruchika@siesgst.ac.in>
2023-06-04 22:37:02 +0530

    created landing , login pgs

---
+++ src/screens/LandingPage/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native/types';
+
+function LandingPage() {
+  return (
+    <View>
+      <Text>THis is landing page</Text>
+    </View>
+  );
+}
+
+export default LandingPage;
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+function LoginPage() {
+  return (
+    <View>
+      <Text>Hey this is</Text>
+    </View>
+  );
+}
+
+export default LoginPage;
